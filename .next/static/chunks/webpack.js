363c4f4ab0b8bcd3eaf6d5079d0165bd8541426f
c1c3ru <cicerosilva.ifce@gmail.com>
2025-11-03 14:34:29 -0300

    teste

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5e650d73b0c470ca"; }
+/******/ 		__webpack_require__.h = function() { return "9962152435a3e66d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
