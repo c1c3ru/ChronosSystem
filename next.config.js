2fdac012b1280a5341340f82480df5dbbead353e
c1c3ru <cicerosilva.ifce@gmail.com>
2025-11-26 13:48:10 -0300

    fix(csp): permitir data: e blob: em connect-src para @react-pdf/renderer

--- next.config.js
+++ next.config.js
@@ -46,7 +46,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https:; font-src 'self' data:; connect-src 'self' https:; frame-src 'self' blob: data:; object-src 'self' blob:;"
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https:; font-src 'self' data:; connect-src 'self' https: data: blob:; frame-src 'self' blob: data:; object-src 'self' blob:;"
           },
           {
             key: 'X-Frame-Options',
